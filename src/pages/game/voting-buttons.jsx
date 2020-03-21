b690dadfce29221a3b80872a45824114f5848124
Nick Cacace <NickCacace@gmail.com>
2020-03-21 18:37:45 -0400

    feat: add points buttons

---
+++ src/pages/game/voting-buttons.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export function VotingButtons({ onSelected }) {
+  const choices = [
+    {
+      value: 0
+    },
+    {
+      value: 1
+    },
+    {
+      value: 2
+    },
+    {
+      value: 3
+    },
+    {
+      value: 5
+    },
+    {
+      value: 8
+    },
+    {
+      value: 13
+    },
+    {
+      value: 20
+    },
+    {
+      value: 40
+    },
+    {
+      value: 100
+    }
+  ];
+
+  return (
+    <div className="buttons">
+      {choices.map(choice => (
+        <button key={choice.value} className="button is-primary is-light" onClick={() => onSelected(choice.value)}>
+          {choice.label || `${choice.value} Points`}
+        </button>
+      ))}
+    </div>
+  );
+}
