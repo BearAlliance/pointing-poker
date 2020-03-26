4263c550138422bf2686f083e0f0e9d53d26b03e
Brian Olore <brian@olore.net>
2020-03-26 16:24:26 -0400

    feat: add defend-your-points section

---
+++ src/pages/game/point-defender-info.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import random from 'lodash.random';
+
+export function PointDefenderInfo({ info }) {
+  function randomize(arr) {
+    return arr[random(0, arr.length - 1)];
+  }
+
+  function randomDeciderName() {
+    let names = [
+      'The machine learning algorithm',
+      'The cloud',
+      'The Great Oz',
+      'The creator of this app',
+      'Your scrum master',
+      'Your scrum lord'
+    ];
+    return randomize(names);
+  }
+
+  if (info.min === info.max || info.lowPeople.length === 0 || info.highPeople.length === 0) {
+    return '';
+  }
+
+  let lowPerson = randomize(info.lowPeople);
+  let highPerson = randomize(info.highPeople);
+
+  return (
+    <div>
+      <div className="has-text-weight-bold is-size-4">Defend your points!</div>
+      <div>
+        <span className="is-italic">{randomDeciderName()}</span> has chosen{' '}
+        <span className="has-text-weight-bold">{highPerson.name}</span> to defend their{' '}
+        <span className="has-text-weight-bold">{info.max}</span> against{' '}
+        <span className="has-text-weight-bold">{lowPerson.name}</span> and their{' '}
+        <span className="has-text-weight-bold">{info.min}</span>
+      </div>
+    </div>
+  );
+}
