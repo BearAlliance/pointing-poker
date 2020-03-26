9597cb8f5762c2f35cc257e80e0033475dbfa828
Brian Olore <brian@olore.net>
2020-03-26 16:24:26 -0400

    feat: add defend-your-points section

--- src/pages/game/game-stats.jsx
+++ src/pages/game/game-stats.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hasEveryoneVoted } from './player-stats';
+import { PointDefenderInfo } from './point-defender-info';
 
 export function GameStats({ players }) {
   // only count people that voted
@@ -28,7 +29,16 @@ export function GameStats({ players }) {
     let highPeople = players.filter(player => {
       return player.points === max;
     });
-    return `${max} by ${highPeople.map(p => p.name)}`;
+
+    return {
+      max,
+      highPeople
+    };
+  }
+
+  function getHighPeopleText() {
+    let { max, highPeople } = getHighPeople();
+    return `${max} by ${highPeople.map(p => p.name).join(', ')}`;
   }
 
   function getLowPeople() {
@@ -38,32 +48,60 @@ export function GameStats({ players }) {
     let lowPeople = players.filter(player => {
       return player.points === min;
     });
-    return `${min} by ${lowPeople.map(p => p.name)}`;
+
+    return {
+      min,
+      lowPeople
+    };
+  }
+
+  function getLowPeopleText() {
+    let { min, lowPeople } = getLowPeople();
+    return `${min} by ${lowPeople.map(p => p.name).join(', ')}`;
+  }
+
+  function defenderInfo() {
+    let { max, highPeople } = getHighPeople();
+    let { min, lowPeople } = getLowPeople();
+
+    return {
+      lowPeople,
+      highPeople,
+      max,
+      min
+    };
+  }
+
+  if (!hasEveryoneVoted(players)) {
+    return '';
   }
 
   return (
-    <table className="table is-striped">
-      <thead>
-        <tr>
-          <th colSpan="2" className="has-text-centered">
-            Stats
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Average</td>
-          <td>{hasEveryoneVoted(players) ? getAveragePoints() : '--'}</td>
-        </tr>
-        <tr>
-          <td>High</td>
-          <td>{hasEveryoneVoted(players) ? getHighPeople() : '--'}</td>
-        </tr>
-        <tr>
-          <td>Low</td>
-          <td>{hasEveryoneVoted(players) ? getLowPeople() : '--'}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <table className="table is-striped">
+        <thead>
+          <tr>
+            <th colSpan="2" className="has-text-centered">
+              Stats
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Average</td>
+            <td>{getAveragePoints()}</td>
+          </tr>
+          <tr>
+            <td>High</td>
+            <td>{getHighPeopleText()}</td>
+          </tr>
+          <tr>
+            <td>Low</td>
+            <td>{getLowPeopleText()}</td>
+          </tr>
+        </tbody>
+      </table>
+      <PointDefenderInfo info={defenderInfo()}></PointDefenderInfo>
+    </div>
   );
 }
