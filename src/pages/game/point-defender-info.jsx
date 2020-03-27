138d8c9573f183074998e3ef366182718786c4a2
Brian Olore <brian@olore.net>
2020-03-27 15:51:30 -0400

    fix: don't change decider name on game changes

--- src/pages/game/point-defender-info.jsx
+++ src/pages/game/point-defender-info.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import random from 'lodash.random';
 
 export function PointDefenderInfo({ info }) {
+  const [deciderName] = useState(getRandomName());
+
+  const highPerson = randomize(info.highPeople);
+  const lowPerson = randomize(info.lowPeople);
+
   function randomize(arr) {
     return arr[random(0, arr.length - 1)];
   }
 
-  function randomDeciderName() {
-    let names = [
+  function getRandomName() {
+    return randomize([
       'The machine learning algorithm',
       'The cloud',
       'The Great Oz',
       'The creator of this app',
       'Your scrum master',
       'Your scrum lord'
-    ];
-    return randomize(names);
+    ]);
   }
 
   if (info.min === info.max || info.lowPeople.length === 0 || info.highPeople.length === 0) {
     return '';
   }
 
-  let lowPerson = randomize(info.lowPeople);
-  let highPerson = randomize(info.highPeople);
-
   return (
     <div>
       <div className="has-text-weight-bold is-size-4">Defend your points!</div>
       <div>
         <div>
-          <span className="is-italic">{randomDeciderName()}</span> has chosen
+          <span className="is-italic">{deciderName}</span> has chosen
         </div>
         <div>
           <span className="has-text-weight-bold">{highPerson.name}</span> to defend their{' '}
