f78f12736c11b40a5178f4809810415a7562ceec
Nick Cacace <NickCacace@gmail.com>
2020-03-26 21:49:36 -0400

    feat: add game link

--- src/pages/game/game-title.jsx
+++ src/pages/game/game-title.jsx
@@ -1,14 +1,22 @@
 import React, { Fragment } from 'react';
+import { isDevelopment } from '../../env';
+
+function gameLink(gameId) {
+  const href = isDevelopment()
+    ? `http://${window.location.host}/game/${gameId}`
+    : `https://${window.location.host}/game/${gameId}`;
+  return <a href={href}>#{gameId}</a>;
+}
 
 export function GameTitle({ gameId }) {
   return (
     <Fragment>
       <div className="hero is-hidden-touch">
         <div className="hero-body">
-          <h1 className="is-size-3">Planning Poker #{gameId}</h1>
+          <h1 className="is-size-3">Planning Poker {gameLink(gameId)}</h1>
         </div>
       </div>
-      <div className="is-hidden-desktop is-size-4">Planning Poker #{gameId}</div>
+      <div className="is-hidden-desktop is-size-4">Planning Poker {gameLink(gameId)}</div>
     </Fragment>
   );
 }
