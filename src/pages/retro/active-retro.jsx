12881e170a7f79f201056e33cd14f10c13cea932
Nick Cacace <NickCacace@gmail.com>
2020-04-05 17:04:28 -0400

    feat(retro): add lonley link when there's only one player

--- src/pages/retro/active-retro.jsx
+++ src/pages/retro/active-retro.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import get from 'lodash.get';
 import { COLUMN_TYPES, RetroColumn } from './retro-column';
 import { RetroSocket } from '../../websocket-client';
+import { InviteLink } from '../../components/invite-link';
 
 let socket;
 
 export function ActiveRetro({ playerName, retroId }) {
-  const [retro, setRetro] = useState([]);
+  const [retro, setRetro] = useState(null);
 
   useEffect(() => {
     socket = new RetroSocket(retroId, playerName, 'retro', {
@@ -23,29 +24,40 @@ export function ActiveRetro({ playerName, retroId }) {
 
   console.log('retro', retro);
 
-  return (
-    <div className="columns">
-      <div className="column">
-        <RetroColumn
-          type={COLUMN_TYPES.KEEP_DOING}
-          addItem={text => socket.addItem(text, COLUMN_TYPES.KEEP_DOING)}
-          items={get(retro, `columns.${COLUMN_TYPES.KEEP_DOING}`, [])}
-        />
-      </div>
-      <div className="column">
-        <RetroColumn
-          type={COLUMN_TYPES.STOP_DOING}
-          addItem={text => socket.addItem(text, COLUMN_TYPES.STOP_DOING)}
-          items={get(retro, `columns.${COLUMN_TYPES.STOP_DOING}`, [])}
-        />{' '}
-      </div>
-      <div className="column">
-        <RetroColumn
-          type={COLUMN_TYPES.START_DOING}
-          addItem={text => socket.addItem(text, COLUMN_TYPES.START_DOING)}
-          items={get(retro, `columns.${COLUMN_TYPES.START_DOING}`, [])}
-        />{' '}
-      </div>
-    </div>
-  );
+  if (retro)
+    return (
+      <Fragment>
+        {retro.players.length === 1 && (
+          <div className="notification is-info is-light">
+            You look lonely. Invite some friends with this link:{' '}
+            <InviteLink id={retroId} type="retro" showHref={true} />{' '}
+          </div>
+        )}
+        <div className="columns">
+          <div className="column">
+            <RetroColumn
+              type={COLUMN_TYPES.KEEP_DOING}
+              addItem={text => socket.addItem(text, COLUMN_TYPES.KEEP_DOING)}
+              items={get(retro, `columns.${COLUMN_TYPES.KEEP_DOING}`, [])}
+            />
+          </div>
+          <div className="column">
+            <RetroColumn
+              type={COLUMN_TYPES.STOP_DOING}
+              addItem={text => socket.addItem(text, COLUMN_TYPES.STOP_DOING)}
+              items={get(retro, `columns.${COLUMN_TYPES.STOP_DOING}`, [])}
+            />{' '}
+          </div>
+          <div className="column">
+            <RetroColumn
+              type={COLUMN_TYPES.START_DOING}
+              addItem={text => socket.addItem(text, COLUMN_TYPES.START_DOING)}
+              items={get(retro, `columns.${COLUMN_TYPES.START_DOING}`, [])}
+            />{' '}
+          </div>
+        </div>
+      </Fragment>
+    );
+
+  return null;
 }
