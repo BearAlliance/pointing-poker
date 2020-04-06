0fd16551bf9570335c01b6a26d2d253fa0cd6081
Nick Cacace <NickCacace@gmail.com>
2020-04-05 21:16:33 -0400

    feat(retro): improve retro item layout

--- src/pages/retro/retro-item.jsx
+++ src/pages/retro/retro-item.jsx
@@ -6,34 +6,46 @@ export function RetroItem({ item, onRemove, onLike, onUnlike, hasVoted }) {
   const [isVoteHover, setIsVoteHover] = useState(null);
 
   return (
-    <div key={item.id} className="notification is-info-light">
-      <div>{item.text}</div>
-      <div className="has-text-right has-text-grey">
-        {item.votes.length}
-        <span className="icon">
-          <i
-            title={hasVoted ? 'Unlike this item' : 'Like this item'}
-            style={{ cursor: 'pointer' }}
-            className={classNames('fa-thumbs-up', {
-              'has-text-grey-dark': isVoteHover,
-              fas: hasVoted,
-              far: !hasVoted
-            })}
-            onMouseEnter={() => setIsVoteHover(true)}
-            onMouseLeave={() => setIsVoteHover(false)}
-            onClick={() => (hasVoted ? onUnlike() : onLike())}
-          />
-        </span>
-        <span className="icon">
-          <i
-            title="Remove item"
-            style={{ cursor: 'pointer' }}
-            className={classNames('far', 'fa-trash-alt', { 'has-text-grey-dark': isTrashHover })}
-            onMouseEnter={() => setIsTrashHover(true)}
-            onMouseLeave={() => setIsTrashHover(false)}
-            onClick={onRemove}
-          />
-        </span>
+    <div className="box">
+      <div className="media">
+        <div className="media-content">
+          <p>
+            <strong>{item.playerId}</strong>
+          </p>
+          <p>{item.text}</p>
+        </div>
+      </div>
+      <div className="has-text-grey columns has-text-right">
+        <div className="column is-two-thirds" />
+        <div className="column">
+          <span className="icon">
+            <i
+              title={hasVoted ? 'Unlike this item' : 'Like this item'}
+              style={{ cursor: 'pointer' }}
+              className={classNames('fa-thumbs-up', {
+                'has-text-grey-dark': isVoteHover,
+                fas: hasVoted,
+                far: !hasVoted
+              })}
+              onMouseEnter={() => setIsVoteHover(true)}
+              onMouseLeave={() => setIsVoteHover(false)}
+              onClick={() => (hasVoted ? onUnlike() : onLike())}
+            />
+          </span>
+          {item.votes.length}
+        </div>
+        <div className="column">
+          <span className="icon">
+            <i
+              title="Remove item"
+              style={{ cursor: 'pointer' }}
+              className={classNames('far', 'fa-trash-alt', { 'has-text-grey-dark': isTrashHover })}
+              onMouseEnter={() => setIsTrashHover(true)}
+              onMouseLeave={() => setIsTrashHover(false)}
+              onClick={onRemove}
+            />
+          </span>
+        </div>
       </div>
     </div>
   );
